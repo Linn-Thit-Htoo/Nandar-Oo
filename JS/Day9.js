418e231975e4d6c74a748f4e9bc006ac1a478479
Linn Thit <lth1212001@gmail.com>
2024-04-28 21:54:37 +0630

    Day 9

---
+++ JS/Day9.js
@@ -0,0 +1,23 @@
+document.write("Hello JS!");
+
+// log
+console.log("Hi");
+console.info("Info")
+console.warn("Warning!");
+console.error("error!");
+
+let userName = "Leo";
+var age = 19;
+const id = 1063;
+
+console.log("userName is: ", userName);
+console.log("age is: ", age);
+console.log("id is: ", { id });
+
+// cocatenation
+let firstName = "Linn Thit ";
+let lastName = "Htoo";
+console.log("Full name: ", firstName + lastName)
+
+// interpolation
+console.log(`Interpolation example: ${firstName + lastName}`);
